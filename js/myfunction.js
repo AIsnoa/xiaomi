92bf617630112fc525d0e7fc9d6b8f2c17da48c5
lianjie <lianjie2@100tal.com>
2021-02-10 19:42:06 +0800

    if and apply

--- js/myfunction.js
+++ js/myfunction.js
@@ -1,37 +1,51 @@
 "use strict"
 
-function debounce(callback,time){
-    console.log("in debounce")
-   var timer=null;
+function debounce(callback,time,immediate){
+    if (typeof callback != "function") throw new TypeError("callback must be an function")
+    if (typeof time == "boolean"){
+        time = 300
+        immediate = time
+    }
+    if(typeof time != "number") time = 300
+    if(typeof immediate != "boolean") immediate = false
+    var timer=null;
     return function(){
+            var runnow = !timer && immediate
+                    params = [].slice.call(arguments)
+                    self = this
+            runnow ? callback.apply(self,params) :null
             if(timer != null){
                 clearInterval(timer)
                 timer = null
             }
             timer =  setTimeout(()=>{
-                callback()
+                !immediate ? callback.apply(self,params): null
                 timer = null
             },time)
+            
     }
 }
 
 function throttle(callback,time){
+    if (typeof callback != "function") throw new TypeError("callback must be an function")
+    if(typeof time != "number") time = 300
     var timer = null;
     var perv = +new Date();
 
     return function(){
-
+    var self = this
+            params = [].slice.call(arguments)
     var  now = +new Date()
     var  remin = now - perv;
         if(remin > time){
             console.log(remin,'fist')
             perv = +new Date()
-            callback()
+            callback.apply(self,params)
         }else if(timer == null){
             timer = setTimeout(()=>{
                 console.log('time out ')
                 timer = null
-                callback()
+                callback.apply(self,params)
             },time-remin)
         }               
         return 
